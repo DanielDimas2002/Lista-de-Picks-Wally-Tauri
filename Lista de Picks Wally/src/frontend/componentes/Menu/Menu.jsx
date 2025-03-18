4a1446f14bbf48d5d1ec7cd00156b05f57c9851c
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2025-03-18 09:19:47 -0300

    Destaque no botão de adicionar

--- Lista de Picks Wally/src/frontend/componentes/Menu/Menu.jsx
+++ Lista de Picks Wally/src/frontend/componentes/Menu/Menu.jsx
@@ -7,17 +7,17 @@ const Menu = ({paginaAtual, onBotaoClick}) => {
 
     const botoes = {
         picks: [
-          { label: "Adicionar", action: "adicionar" },
+          { label: "Adicionar", action: "adicionar", destaque: true},
           { label: "Vidas do Chat", action: "vidas" },
           { label: "Banco", action: "banco" },
         ],
         vidas: [
-          { label: "Adicionar", action: "adicionar" },
+          { label: "Adicionar", action: "adicionar", destaque: true},
           { label: "Picks do Chat", action: "picks" },
           { label: "Banco", action: "banco" },
         ],
         banco: [
-          { label: "Adicionar", action: "adicionar" },
+          { label: "Adicionar", action: "adicionar", destaque: true },
           { label: "Picks do Chat", action: "picks" },
           { label: "Vidas do Chat", action: "vidas" },
         ],
@@ -28,7 +28,11 @@ const Menu = ({paginaAtual, onBotaoClick}) => {
             <h1>PICKS DO CHAT</h1>
             <div className="menu-botoes">
                 {botoes[paginaAtual]?.map((botao)=>(
-                    <button key={botao.action} onClick={() => onBotaoClick(botao.action)}>{botao.label}</button>
+                    <button
+                    key={botao.action}
+                    onClick={() => onBotaoClick(botao.action)}
+                    className={botao.destaque ? "botao-adicionar" : ""}
+                  >{botao.label}</button>
                 ))}
             </div>
         </div>
