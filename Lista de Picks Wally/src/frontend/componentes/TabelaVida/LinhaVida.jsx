9efb6e29c7f0680f0f7cfad4521b55b77ca20d23
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2025-03-18 08:42:20 -0300

    Adição de ícones no lugar dos botões

--- Lista de Picks Wally/src/frontend/componentes/TabelaVida/LinhaVida.jsx
+++ Lista de Picks Wally/src/frontend/componentes/TabelaVida/LinhaVida.jsx
@@ -10,12 +10,29 @@ const LinhaVida = ({ index, nome, vida, winRate, vitorias, derrotas, totalVidas,
       <div className="coluna">{derrotas}</div>
       <div className="coluna">{totalVidas}</div>
       <div className="coluna">
-        <button onClick={() => onAdicionarVida(index)}>+Vida</button>
-        <button onClick={() => onReduzirVida(index)}>-Vida</button>
-        <button onClick={() => onVitoria(index)}>Vit</button>
-        <button onClick={() => onDerrota(index)}>Der</button>
-        <button onClick={() => onLimpar(index)}>Limpar</button>
-        <button onClick={() => onExcluir(index)}>Excluir</button>
+        <button onClick={() => onAdicionarVida(index)} title="Adicionar uma vida">
+          <span className="material-symbols-outlined">add</span>
+        </button>
+
+        <button onClick={() => onReduzirVida(index)} title="Reduzir uma vida">
+          <span className="material-symbols-outlined">remove</span>
+        </button>
+
+        <button onClick={() => onVitoria(index)} title="Registrar uma vitória">
+          <span className="material-symbols-outlined">emoji_events</span>
+        </button>
+
+        <button onClick={() => onDerrota(index)} title="Registrar uma derrota">
+          <span className="material-symbols-outlined">thumb_down</span>
+        </button>
+
+        <button onClick={() => onLimpar(index)} title="Zerar vidas do jogador">
+          <span className="material-symbols-outlined">restart_alt</span>
+        </button>
+
+        <button onClick={() => onExcluir(index)} title="Remover jogador da lista">
+          <span className="material-symbols-outlined">delete</span>
+        </button>
       </div>
     </div>
   );
