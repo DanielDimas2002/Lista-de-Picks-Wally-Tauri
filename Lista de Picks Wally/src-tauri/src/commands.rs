697ec147bbc54fbc1dec0d4cbfc1505b7b166c61
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2025-03-19 09:38:52 -0300

    Configuração do backend

--- Lista de Picks Wally/src-tauri/src/commands.rs
+++ Lista de Picks Wally/src-tauri/src/commands.rs
@@ -4,6 +4,10 @@ use tauri::Manager;
 use std::sync::Mutex;
 use std::collections::HashMap;
 
+use crate::data::{carregar_dados, salvar_dados, inicializar_arquivos, Pick, VidaChat, Banco};
+use std::io;
+
+
 #[derive(Default)]
 struct AppState {
     personagens: Mutex<HashMap<String, commands::Personagem>>,
@@ -19,4 +23,34 @@ fn main() {
         ])  // Registrando os comandos
         .run(tauri::generate_context!())
         .expect("Erro ao rodar o Tauri");
+}
+
+#[tauri::command]
+pub fn get_picks() -> Result<Vec<Pick>, String> {
+    carregar_dados::<Vec<Pick>>("picks.json").map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+pub fn set_picks(picks: Vec<Pick>) -> Result<(), String> {
+    salvar_dados("picks.json", &picks).map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+pub fn get_vidas_chat() -> Result<Vec<VidaChat>, String> {
+    carregar_dados::<Vec<VidaChat>>("vidas_chat.json").map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+pub fn set_vidas_chat(vidas_chat: Vec<VidaChat>) -> Result<(), String> {
+    salvar_dados("vidas_chat.json", &vidas_chat).map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+pub fn get_banco() -> Result<Vec<Banco>, String> {
+    carregar_dados::<Vec<Banco>>("banco.json").map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+pub fn set_banco(banco: Vec<Banco>) -> Result<(), String> {
+    salvar_dados("banco.json", &banco).map_err(|e| e.to_string())
 }
